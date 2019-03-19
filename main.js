69242fc399a200fb05c09d023e2fb6e004ec9116
Predrag <idmpepe@gmail.com>
2019-03-19 15:44:57 +0100

    Randomize the array

--- main.js
+++ main.js
@@ -33,6 +33,15 @@ let uniqueMatches = [
 function generateCardsHtml() {
 	let mathces = [...uniqueMatches, ...uniqueMatches];
 
+	function shuffle(a) {
+		for (let i = a.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[a[i], a[j]] = [a[j], a[i]];
+		}
+		return a;
+	}
+	shuffle(mathces);
+
 	console.log(mathces);
 
 	return mathces.map(value => Card({ value })).join('');
