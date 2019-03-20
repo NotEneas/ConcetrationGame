df36e404334ef26c0ccfbd8c1d1f5c543b624cf9
Predrag <idmpepe@gmail.com>
2019-03-20 17:18:25 +0100

    dasd

--- sw.js
+++ sw.js
@@ -1,12 +1,7 @@
 self.addEventListener('install', e => {
 	e.waitUntil(
 		caches.open('concentration_game').then(cache => {
-			return cache.addAll([
-				'/',
-				'/index.html',
-				'./main.css',
-				'./main.js'
-			]);
+			return cache.addAll(['/', '/index.html', 'main.css', 'main.js']);
 		})
 	);
 });
